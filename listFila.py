cd540acd092b3a54ad7a1ba01428535d02310b83
rodrigossbjj <rodrigosousasales9@gmail.com>
2025-09-26 09:28:24 -0300

    Continuando problemas com Filas (Queue)

--- listFila.py
+++ listFila.py
@@ -2,7 +2,7 @@ from stack import *
 from fila import * 
 
 # Inverter Fila com Pilhas
-def inverterFila(a:Queue):
+def inverterFilaComPilha(a:Queue):
     if a.isEmpty():
         return None 
     s = Stack()
@@ -28,3 +28,30 @@ def inverterPilha(s:Stack):
         s.push(q.deQueue())
 
     return s
+
+# Inverter K primeiros elementos de uma Fila
+def inverterKEl(q:Queue, k):
+    if q.isEmpty():
+        return None 
+    s = Stack()
+
+    for i in range(k):
+        s.push(q.deQueue())
+    
+    while not s.isEmpty():
+        q.enQueue(s.pop())
+    
+    for i in range(q.size - k):
+        q.enQueue(q.deQueue())
+    
+
+# Implemente uma Fila usando duas Pilhas (Não sei)
+class queueWS(object):
+    def __init__(self, s1:Stack, s2:Stack, limit=5):
+        pass
+    def enQueue(self, data):
+        self.s1.push(data)
+    def deQueue(self):  
+        if self.s2.isEmpty():
+            while self.s1.isEmpty(): self.s2.push(self.s1.pop())
+        return self.s2.pop()

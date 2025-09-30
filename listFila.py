3d2397fa84682950808684e791edca4f73bdc695
rodrigossbjj <rodrigosousasales9@gmail.com>
2025-09-30 18:40:30 -0300

    Inicio lista de filas

--- listFila.py
+++ listFila.py
@@ -55,3 +55,39 @@ class queueWS(object):
         if self.s2.isEmpty():
             while self.s1.isEmpty(): self.s2.push(self.s1.pop())
         return self.s2.pop()
+
+
+# --------INICIA A LISTA 4---------------
+
+# QUESTÃO 4
+# Considere um deque D que contém os números (1,2,3,4,5,6,7,8), nessa ordem. 
+# Suponha ainda que há uma fila Q, inicialmente vazia. Elabore um trecho de código que usando 
+# apenas D e Q (sem variáveis adicionais) e resulte em Q armazenando os elementos na 
+# ordem (1,2,3,4,5,6,7,8).
+def transfereDequeParaQueue(D, Q:Queue):
+    while not D.isEmpty():
+        elem = D.delete_first()
+        Q.enQueue(elem)
+
+# QUESTÃO 5
+# Implemente uma fila através de duas pilhas. Não crie variáveis, use apenas as operações das 
+# pilhas e não use recursão.
+class QueueTwoStacks:
+    def __init__(self, capacity=10):
+        self.S1 = Stack(capacity)
+        self.S2 = Stack(capacity)
+
+    def enQueue(self, item):
+        while not self.S1.isEmpty():
+            self.S2.push(self.S1.pop())
+
+        self.S1.push(item)
+
+        while not self.S2.isEmpty():
+            self.S1.push(self.S2.pop())
+
+    def deQueue(self):
+        if self.S1.isEmpty():
+            print("Fila vazia")
+            return None
+        return self.S1.pop()

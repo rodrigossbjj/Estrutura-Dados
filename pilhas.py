61a35c497a83b62d227e159eb363780d75565b20
rodrigossbjj <rodrigosousasales9@gmail.com>
2025-10-06 16:15:43 -0300

    Continuação listPilhas

--- pilhas.py
+++ pilhas.py
@@ -32,3 +32,48 @@ def inverteVetor(arr):
     while not s.isEmpty():
         arr.append(s.pop())
     return arr
+
+# QUESTÃO 6
+# Seja P uma pilha de inteiros não negativos ocupando um array A com N posições, de modo que:
+# Números empilhados em P ocupam posições crescentes de A a partir de A[1]
+# A[0] contém o índice em A do número empilhado por último (topo da pilha)
+# A pilha pode crescer enquanto houver posições em A.
+# Implemente as funções de acesso às pilhas: push, pop, peek, len, isEmpty, isFull, empty( esvazia a pilha).
+N = 6 
+def init():
+    A = [0] * N  
+    A[0] = 0     
+    return A
+
+def isEmpty(A):
+    return A[0] == 0
+
+def isFull(A):
+    return A[0] == N - 1
+
+def push(A, x):
+    if isFull(A):
+        print("Erro: pilha cheia!")
+        return
+    A[0] += 1
+    A[A[0]] = x
+
+def pop(A):
+    if isEmpty(A):
+        print("Erro: pilha vazia!")
+        return None
+    valor = A[A[0]]
+    A[0] -= 1
+    return valor
+
+def peek(A):
+    if isEmpty(A):
+        print("Erro: pilha vazia!")
+        return None
+    return A[A[0]]
+
+def length(A):
+    return A[0]
+
+def empty(A):
+    A[0] = 0
